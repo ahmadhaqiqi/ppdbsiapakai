02f8fa23a7b9caddf87e484ee1ab15cdff181175
ahmadhaqiqi <ahmadhaqiqi61@admin.sd.belajar.id>
2026-06-10 01:20:19 +0700

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 // Service to interact with Google Apps Script Backend
 
 // To use the real backend, replace this URL with your deployed Google Apps Script Web App URL
-const GAS_WEB_APP_URL = ""; 
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzr7QjrKFqgBsbsAlWXTqKos2i6bVbvb-NhzRMwtIkDXgu3J1sTPVJVXZ3USvZyWAjw/exec"; 
 
 export interface FormField {
   id: string;
